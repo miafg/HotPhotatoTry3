7df73b071734e99cff73f6c63718bc59c17173e6
miafg <miaf@sas.upenn.edu>
2015-10-12 11:14:55 -0400

    Created add friends table view controller

---
+++ HotPhotato/AddFriendsVC.swift
@@ -0,0 +1,41 @@
+//
+//  AddFriendsVC.swift
+//  HotPhotato
+//
+//  Created by Mia Froehling-Gallier on 10/12/15.
+//  Copyright © 2015 hotPhotato. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendsVC: UITableViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func cancelAddFriendsVC(segue:UIStoryboardSegue) {
+    }
+    
+    @IBAction func saveAddedFriend(segue:UIStoryboardSegue) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
